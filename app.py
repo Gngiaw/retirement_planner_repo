0da6ba060646d1e69a09da9610147edb0fa14509
Gngiaw <gladys.ngiaw@yahoo.com>
2025-05-13 16:48:28 +0800

    Update app.py

--- app.py
+++ app.py
@@ -259,7 +259,7 @@ for rate in rates:
         balance += year_monthly_total + year_lump_total
 
         # apply 1 year compound growth
-        balance *= (1 + rate)
+        balance *= fv(1 + rate)
 
         # store result
         balances.append(balance)
